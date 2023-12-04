526d5c67145f3f357756e64b5f5cb5b54d9655d8
arceus <islom4ik.numanov@gmail.com>
2023-12-04 11:35:10 +0900

    [day-3] p1&2

--- 2023/day_3_gear_ratios.py
+++ 2023/day_3_gear_ratios.py
@@ -1,10 +1,14 @@
 # ------ Gear Ratios ------
+import math as m, re
+
+
 ### Test
 def isSym(inp):
-    if inp != '.' and inp != None and not inp.isdigit():
+    if inp != "." and inp != None and not inp.isdigit():
         return True
     else:
         return False
+
 
 def fTest():
     tFile = open("day_3_test.txt")
@@ -12,14 +16,21 @@ def fTest():
 
     data = []
     for line in tFile:
+        print("tFile >", line)
         line = line.replace("\n", "")
         data.append(line)
 
     for ridx, line in enumerate(data):
+        pRow = None
+        nRow = None
+        if ridx > 0:
+            pRow = list(data[ridx - 1])
+        if ridx < len(data) - 1:
+            nRow = list(data[ridx + 1])
         row = list(data[ridx])
-        print("row", row)
+        print("curr row", row)
 
-        target = ''
+        target = ""
         tStart = None
         tEnd = None
 
@@ -37,43 +48,98 @@ def fTest():
                 if tStart is None:
                     tStart = idx
 
-                if not prev == '.' and not prev == None:
+                if not prev == "." and not prev == None:
                     lSym = True
 
                 if nxt.isdigit():
-                    tEnd = idx+1
+                    tEnd = idx + 1
                 else:
-                    print('col >>>', col)
-                    print('target >>>', target)
-                    tEnd = idx
-                    if nxt != '.' and nxt != None:
+                    if idx < len(row) - 1:
+                        tEnd = idx + 1
+                    else:
+                        tEnd = idx
+                    if nxt != "." and nxt != None:
                         rSym = True
-                    if ridx < len(data) - 1:
-                        nxtRow = data[ridx + 1]
-                        print('nxtRow', nxtRow)
-                        print('nxtRow[idx + 1]', nxtRow[idx + 1])
-                        if isSym(nxtRow[idx]) or isSym(nxtRow[tStart]) or isSym(nxtRow[idx + 1]):
-                            print('yaaaaaaaaaaaaaaaay')
-                            bSym = True
-                        print('lSym', lSym)
-                        print('rSym', rSym)
-                        print('bSym', bSym)
+                print("tStart", tStart)
+                print("tEnd", tEnd)
+                # Check above and below rows
+                if tStart != None and tEnd != None:
+                    if pRow != None:
+                        for pVal in range(tStart - 1, tEnd + 1):
+                            print("pVal", pVal)
+                            if isSym(pRow[pVal]):
+                                tSym = True
+                    if nRow != None:
+                        for pVal in range(tStart - 1, tEnd + 1):
+                            if isSym(nRow[pVal]):
+                                bSym = True
+            else:
+                if lSym or rSym or tSym or bSym:
+                    print("target", target)
+                    out.append(target)
+                target = ""
+                lSym = False
+                rSym = False
+                tSym = False
+                bSym = False
+                tStart = None
+                tEnd = None
 
-                    if lSym or rSym or bSym:
-                        print('target', target)
-                        out.append(target)
-                    target = ''
-                    lSym = False
-                    rSym = False
-                    bSym = False
-                    tStart = None
-                    tEnd = None
+                # if ridx < len(data) - 1:
+                #     nxtRow = data[ridx + 1]
+                #     print('nxtRow', nxtRow)
+                #     print('nxtRow[idx + 1]', nxtRow[idx + 1])
+                #     if isSym(nxtRow[idx]) or isSym(nxtRow[tStart]) or isSym(nxtRow[idx + 1]):
+                #         print('yaaaaaaaaaaaaaaaay')
+                #         bSym = True
+                #     print('lSym', lSym)
+                #     print('rSym', rSym)
+                #     print('bSym', bSym)
+
+                # if lSym or rSym or bSym:
+                #     print('target', target)
+                #     out.append(target)
+                # target = ''
+                # lSym = False
+                # rSym = False
+                # bSym = False
+                # tStart = None
+                # tEnd = None
     return out
 
-        #if ridx < len(data) - 1:
-        #    nxtRow = data[ridx + 1]
-        #    print("nr >>>", nxtRow)
+    # if ridx < len(data) - 1:
+    #    nxtRow = data[ridx + 1]
+    #    print("nr >>>", nxtRow)
 
 
-total = fTest()
-print("total", total)
+# total = fTest()
+
+
+def question():
+    board = list(open("day_3.txt"))
+
+    chars = {
+        (r, c): []
+        for r in range(140)
+        for c in range(140)
+        if board[r][c] not in "01234566789."
+    }
+
+    for r, row in enumerate(board):
+        for n in re.finditer(r"\d+", row):
+            edge = {
+                (r, c)
+                for r in (r - 1, r, r + 1)
+                for c in range(n.start() - 1, n.end() + 1)
+            }
+
+            for o in edge & chars.keys():
+                chars[o].append(int(n.group()))
+
+    print(
+        sum(sum(p) for p in chars.values()),
+        sum(m.prod(p) for p in chars.values() if len(p) == 2),
+    )
+
+
+question()
