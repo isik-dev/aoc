2f818f9cdd779bf9a0a0c61116ac32e9ccf9fd5e
arceus <islom4ik.numanov@gmail.com>
2023-12-05 15:19:24 +0900

    [day-4] part 2

--- 2023/day_4_scratchcards.py
+++ 2023/day_4_scratchcards.py
@@ -8,12 +8,11 @@ def fTest():
     cardPoints = []
 
     for idx, card in enumerate(tFile):
-
         headIdx = card.find(":")
         bodyIdx = card.find("|")
 
-        wCards = card[headIdx+1:bodyIdx].split()
-        mC = card[bodyIdx+1:].split()
+        wCards = card[headIdx + 1 : bodyIdx].split()
+        mC = card[bodyIdx + 1 :].split()
 
         myCards = set(mC)
 
@@ -27,15 +26,67 @@ def fTest():
             if match == 1:
                 cardPoints.append(1)
             else:
-                cardPoints.append(2**(match-1))
+                cardPoints.append(2 ** (match - 1))
         print("match", match)
         match = 0
         print("matchOut", matchOut)
     return cardPoints
 
 
+# res = fTest()
+# print("res >>>>>> ", res)
+# print("res sum >> ", sum(res))
 
 
-res = fTest()
-print("res >>>>>> ", res)
-print("res sum >> ", sum(res))
+def questionTwo():
+    out = []
+    details = {}
+
+    # create starter dict
+    mFile = open("day_4.txt", "r")
+    for idx, card in enumerate(mFile):
+        gameIdx = idx + 1
+
+        details[gameIdx] = {"original": 1, "match": 0, "copies": 0}
+
+    tFile = open("day_4.txt", "r")
+    for idx, card in enumerate(tFile):
+        gameIdx = idx + 1
+
+        headIdx = card.find(":")
+        bodyIdx = card.find("|")
+
+        wC = card[headIdx + 1 : bodyIdx].split()
+        mC = card[bodyIdx + 1 :].split()
+
+        mC = set(mC)
+
+        match = 0
+        copies = 0
+
+        for c in wC:
+            if c in mC:
+                match += 1
+        details[gameIdx]["match"] = match
+
+        for i, m in enumerate(range(match)):
+            next = i + 1
+            details[gameIdx + next]["copies"] += 1
+            copies += 1
+        print("gameIdx", gameIdx)
+
+        print('details[gameIdx]["copies"]', details[gameIdx]["copies"])
+        out.append(details[gameIdx]["copies"] + 1)
+
+        for cidx, copy in enumerate(range(details[gameIdx]["copies"])):
+            for mi, m in enumerate(range(match)):
+                next = mi + 1
+                details[gameIdx + next]["copies"] += 1
+
+        print("details", details)
+
+    return out
+
+
+total = questionTwo()
+print("This is the total sum", sum(total))
